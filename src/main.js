cfdcacf3559a841eea394df673dc751f3ddd8b04
nasuda <nasuda908@gmail.com>
2023-12-15 21:38:44 +0900

    add function

--- src/main.js
+++ src/main.js
@@ -3,7 +3,25 @@ console.log('main');
 const app = document.getElementById('app');
 console.log(app);
 
+let state = 0;
+
 
 app.innerHTML = `<h2>app</h2>
   <p>JavaSCript</p>
+  <p class="count">${state}</p>
 `
+
+const count = document.querySelector('.count')
+console.log(count)
+
+function addCount() {
+  state = state + 1;
+}
+
+const addBtn = document.getElementById('add')
+addBtn.addEventListener('click', () => {
+  addCount();
+  count.innerText = state;
+} )
+
+
